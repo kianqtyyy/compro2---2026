59b31435aafed8214fa9620e465487951643aa06
kianqtyyy <kiandaveigh.estigoy@lorma.edu>
2026-01-20 04:39:37 +0800

    row

---
+++ week2/ActivityWeek2.java
@@ -0,0 +1,11 @@
+public class ActivityWeek2 {
+    public static void main(String [] args){
+       int [] theater = new int[8];
+       theater[3] = 1;
+         for(int i = 0; i < theater.length; i++){
+            System.out.println(theater[i]);
+                System.out.printf("%-8s", theater[i]);
+            }
+            System.out.println();
+    }
+}
